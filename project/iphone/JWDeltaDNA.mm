f9f216a0fc64244f480e92f7eceb0d4a60348984
Bill Booth <wrbooth@gmail.com>
2015-10-21 11:37:44 -0400

    removing log lines

--- project/iphone/JWDeltaDNA.mm
+++ project/iphone/JWDeltaDNA.mm
@@ -21,21 +21,16 @@
           eHostname:(NSString *) eHostname 
              userId:(NSString *) userId
 {
-NSLog(@"HERE20");
     DDNASDK *sdk = DDNASDK.sharedInstance;
-NSLog(@"HERE21");
     
     NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
-NSLog(@"HERE22: %@, %@, %@, %@", envKey, cHostname, eHostname, userId);
 
     [sdk startWithEnvironmentKey: envKey
                       collectURL: cHostname
                        engageURL: eHostname
                           userID: userId];
-NSLog(@"HERE23");
 
     sdk.clientVersion = version;
-NSLog(@"HERE24");
 }
 
 
@@ -54,26 +49,17 @@ extern "C"
     
     void jwddInit(const char * sEnvKey, const char * sCHostname, const char * sEHostname, const char * sUserId)
     {
-NSLog(@"HERE1");
         myDd = [[JWDeltaDNA alloc] init];
-NSLog(@"HERE2");
 		NSString *envKey = [[NSString alloc] initWithUTF8String: sEnvKey];
-NSLog(@"HERE3");
 		NSString *cHostname = [[NSString alloc] initWithUTF8String: sCHostname];
-NSLog(@"HERE4");
 		NSString *eHostname = [[NSString alloc] initWithUTF8String: sEHostname];
-NSLog(@"HERE5");
 		NSString *userId = nil;
-NSLog(@"HERE6");
         if (sUserId != NULL)
         {
-NSLog(@"HERE7");
             userId = [[NSString alloc] initWithUTF8String: sUserId];
         }
-NSLog(@"HERE8");
 
         [myDd initWithKey: envKey cHostname: cHostname eHostname: eHostname userId: userId];
-NSLog(@"HERE9");
     }
 
     void jwddRecordEvent(const char * sName, const char * sParams)
